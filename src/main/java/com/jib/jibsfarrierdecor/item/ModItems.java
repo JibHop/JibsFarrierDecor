76f036efe3e1e411ed02600f8b3b4dcf0088ad19
JibHop <enda29003@gmail.com>
2024-06-21 17:49:36 +0100

    Added Horseshoe item

--- src/main/java/com/jib/jibsfarrierdecor/item/ModItems.java
+++ src/main/java/com/jib/jibsfarrierdecor/item/ModItems.java
@@ -1,17 +1,21 @@
 package com.jib.jibsfarrierdecor.item;
 
 import com.jib.jibsfarrierdecor.JibsFarrierDecor;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, JibsFarrierDecor.MOD_ID);
 
+public static final RegistryObject<Item> HORSESHOE = ITEMS.register("horseshoe",
+        () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS)));
 
 public static void register(IEventBus eventBus) {
     ITEMS.register(eventBus);
-}
+    }
 }
