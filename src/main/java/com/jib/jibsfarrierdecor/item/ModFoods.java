1430ba6bb33f7b3420e2cac0f66f1b78b50e110d
JibHop <enda29003@gmail.com>
2024-06-23 23:34:42 +0100

    3 Treats added, not yet edible for animal entities

---
+++ src/main/java/com/jib/jibsfarrierdecor/item/ModFoods.java
@@ -0,0 +1,14 @@
+package com.jib.jibsfarrierdecor.item;
+
+import net.minecraft.world.food.FoodProperties;
+
+public class ModFoods {
+    public static final FoodProperties BANANACARROTAPPLESAUCE_TREATS = (new FoodProperties.Builder()).nutrition(4).saturationMod(0.3F).build();
+
+    public static final FoodProperties COCONUT_TREATS = (new FoodProperties.Builder()).nutrition(3).saturationMod(0.6F).build();
+
+    public static final FoodProperties MINTS = (new FoodProperties.Builder()).nutrition(2).saturationMod(0.1F).build();
+
+
+
+}
--- src/main/java/com/jib/jibsfarrierdecor/item/ModItems.java
+++ src/main/java/com/jib/jibsfarrierdecor/item/ModItems.java
@@ -20,7 +20,17 @@ public static final RegistryObject<Item> HORSESHOE = ITEMS.register("horseshoe",
     public static final RegistryObject<Item> HOOF_NIPPERS = ITEMS.register("hoof_nippers",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FARRIERDECO)));
 
-public static void register(IEventBus eventBus) {
+    public static final RegistryObject<Item> BANANACARROTAPPLESAUCE_TREATS = ITEMS.register("bananacarrotapplesauce_treats",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FARRIERDECO).food(ModFoods.BANANACARROTAPPLESAUCE_TREATS)));
+
+    public static final RegistryObject<Item> COCONUT_TREATS = ITEMS.register("coconut_treats",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FARRIERDECO).food(ModFoods.COCONUT_TREATS)));
+
+    public static final RegistryObject<Item> MINTS = ITEMS.register("mints",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FARRIERDECO).food(ModFoods.MINTS)));
+
+
+    public static void register(IEventBus eventBus) {
     ITEMS.register(eventBus);
     }
 }
