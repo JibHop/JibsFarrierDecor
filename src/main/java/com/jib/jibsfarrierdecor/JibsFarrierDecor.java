738e45fbddfc4820de9c2ef4783c9d8489ca3d9f
JibHop <enda29003@gmail.com>
2024-06-23 20:00:37 +0100

    added Temp Block

--- src/main/java/com/jib/jibsfarrierdecor/JibsFarrierDecor.java
+++ src/main/java/com/jib/jibsfarrierdecor/JibsFarrierDecor.java
@@ -1,5 +1,6 @@
 package com.jib.jibsfarrierdecor;
 
+import com.jib.jibsfarrierdecor.block.ModBlocks;
 import com.jib.jibsfarrierdecor.item.ModItems;
 import com.mojang.logging.LogUtils;
 import net.minecraft.world.level.block.Block;
@@ -31,6 +32,7 @@ public class JibsFarrierDecor
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(eventBus);
+        ModBlocks.register(eventBus);
 
         eventBus.addListener(this::setup);
 
---
+++ src/main/java/com/jib/jibsfarrierdecor/block/ModBlocks.java
@@ -0,0 +1,45 @@
+package com.jib.jibsfarrierdecor.block;
+
+import com.jib.jibsfarrierdecor.JibsFarrierDecor;
+import com.jib.jibsfarrierdecor.item.ModCreativeModeTab;
+import com.jib.jibsfarrierdecor.item.ModItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, JibsFarrierDecor.MOD_ID);
+
+    public static final RegistryObject<Block> TEMP_BLOCK = registerblock("temp_block",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.SAND)
+            ), ModCreativeModeTab.FARRIERDECO);
+
+
+
+
+    private static <T extends Block> RegistryObject<T> registerblock(String name, Supplier<T> block, CreativeModeTab tab) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn, tab);
+        return toReturn;
+    }
+
+
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block, CreativeModeTab tab) {
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
+                new Item.Properties().tab(tab)));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
