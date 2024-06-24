63659bf9af4ca3f85d5d6dc62d1d5f26f369b66c
JibHop <enda29003@gmail.com>
2024-06-24 06:49:51 +0100

    size up hoof jack

--- src/main/java/com/jib/jibsfarrierdecor/JibsFarrierDecor.java
+++ src/main/java/com/jib/jibsfarrierdecor/JibsFarrierDecor.java
@@ -3,6 +3,8 @@ package com.jib.jibsfarrierdecor;
 import com.jib.jibsfarrierdecor.block.ModBlocks;
 import com.jib.jibsfarrierdecor.item.ModItems;
 import com.mojang.logging.LogUtils;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
@@ -11,6 +13,7 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
@@ -38,6 +41,10 @@ public class JibsFarrierDecor
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void clientSetup(final FMLClientSetupEvent event) {
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.BROWN_HOOFJACK.get(), RenderType.solid());
     }
 
     private void setup(final FMLCommonSetupEvent event) {
