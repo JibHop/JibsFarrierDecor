dfe0a6c0a8a0d145ef311c786ab9858cb2d06886
JibHop <enda29003@gmail.com>
2024-08-26 01:21:25 +0100

    fix cyan pick and add translations

--- src/main/java/com/jib/jibsfarrierdecor/block/entity/custom/ToolBoxEntity.java
+++ src/main/java/com/jib/jibsfarrierdecor/block/entity/custom/ToolBoxEntity.java
@@ -1,7 +1,13 @@
 package com.jib.jibsfarrierdecor.block.entity.custom;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class ToolBoxEntity extends BlockEntity {
-    
+
+    public ToolBoxEntity(BlockEntityType<?> pType, BlockPos pPos, BlockState pBlockState) {
+        super(pType, pPos, pBlockState);
+    }
 }
